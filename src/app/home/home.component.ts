95eb4a0376fb36fdb928818122704f4c0f840d58
Jenniffer Cubillos <jrcb@gft.com>
2018-01-18 12:29:31 +0100

    service added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, keyframes, query,stagger } from '@angular/animations';
+import { DataService } from  '../data.service'
 
 @Component({
   selector: 'app-home',
@@ -29,25 +30,26 @@ export class HomeComponent implements OnInit {
 	itemCount: number;
 	btnText: string;
 	goalText: string;
-	goals = ['Myfirst life goarl',
-						'I want to climb a mountain',
-						'Go ice skiing '];
-  constructor() { 
-  	
+	goals = [];
+  constructor(private _data: DataService) {   	
   	this.btnText = 'Add an item';
   	this.goalText = 'My first life goal';
   }
 
   ngOnInit() {
-  	this.itemCount = this.goals.length;
+    this._data.goal.subscribe(res => this.goals = res);
+    this.itemCount = this.goals.length;
+    //this._data.changeGoal(this.goals);
   }
   addItem() {
   	this.goals.push(this.goalText);
   	this.goalText = ' ';
   	this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
   removeItem(i){
   	this.goals.splice(i, 1);
+    this._data.changeGoal(this.goals);
   }
 
 }
